e94b22c6bffb0570ede6840af0a87a5eb34b742a
anthonyrct <anthonyteixaira@gmail.com>
2024-04-02 16:51:48 -0300

    02/04/24

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,13 +1,19 @@
 <?php
 require_once 'conectaBD.php';
-
+// Definir o BD (e a tabela)
+// Conectar ao BD (com o PHP)
 
 if (!empty($_POST)) {
+    // Está chegando dados por POST e então posso tentar inserir no banco
+// Obter as informações do formulário ($_POST)
     try {
+        // Preparar as informações
+// Montar a SQL (pgsql)
         $sql = "INSERT INTO usuario
-        ( nome, data_nascimento, telefone, email, senha)
-        VALUES
-        (:nome, :dataNascimento, :telefone, :email, :senha";
+
+(nome, data_nascimento, telefone, email, senha, produtos)
+VALUES
+(:nome, :dataNascimento, :telefone, :email, :senha, :produtos)";
         // Preparar a SQL (pdo)
         $stmt = $pdo->prepare($sql);
         // Definir/organizar os dados p/ SQL
@@ -16,7 +22,8 @@ if (!empty($_POST)) {
             ':dataNascimento' => $_POST['dataNascimento'],
             ':telefone' => $_POST['telefone'],
             ':email' => $_POST['email'],
-            ':senha' => md5($_POST['senha']) //md5 é um padrão de criptografia
+            ':senha' => md5($_POST['senha']), //md5 é um padrão de criptografia
+            ':produtos' => $_POST['produtos']
         );
         // Tentar Executar a SQL (INSERT)
 // Realizar a inserção das informações no BD (com o PHP)
